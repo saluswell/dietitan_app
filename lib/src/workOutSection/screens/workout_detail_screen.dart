ab3d1116d85610a34398d1be6d0eb81e37825c0f
sohaib37104 <sohaib@softlinks.ae>
2023-09-19 21:58:46 +0500

    dispose video added

--- lib/src/workOutSection/screens/workout_detail_screen.dart
+++ lib/src/workOutSection/screens/workout_detail_screen.dart
@@ -33,104 +33,116 @@ class _WorkoutDetailScreenState extends State<WorkoutDetailScreen> {
   }
 
   @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(
-          "Workout Details",
-          style: TextStyle(fontSize: 15),
+    return WillPopScope(
+      onWillPop: () async {
+        _controller.dispose();
+        return true;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            "Workout Details",
+            style: TextStyle(fontSize: 15),
+          ),
         ),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              height: 200,
-              width: MediaQuery.of(context).size.width,
-              child: ClipRRect(
-                  borderRadius: BorderRadius.only(
-                      topRight: Radius.circular(0),
-                      topLeft: Radius.circular(0),
-                      bottomLeft: Radius.circular(0),
-                      bottomRight: Radius.circular(0)),
-                  child: Stack(
-                    children: [
-                      _controller.value.isInitialized
-                          ? VideoPlayer(_controller)
-                          : SpinKitPouringHourGlass(
-                              size: 30, color: AppColors.appcolor),
-                      Positioned(
-                          child: Center(
-                              child: InkWell(
-                        onTap: () {
-                          if (_controller.value.isPlaying) {
-                            setState(() {
-                              _controller.pause();
-                            });
-                          } else {
-                            setState(() {
-                              _controller.play();
-                            });
-                          }
-                        },
-                        child: Container(
-                          height: 35,
-                          width: 35,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(65),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: 200,
+                width: MediaQuery.of(context).size.width,
+                child: ClipRRect(
+                    borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(0),
+                        topLeft: Radius.circular(0),
+                        bottomLeft: Radius.circular(0),
+                        bottomRight: Radius.circular(0)),
+                    child: Stack(
+                      children: [
+                        _controller.value.isInitialized
+                            ? VideoPlayer(_controller)
+                            : SpinKitPouringHourGlass(
+                                size: 30, color: AppColors.appcolor),
+                        Positioned(
+                            child: Center(
+                                child: InkWell(
+                          onTap: () {
+                            if (_controller.value.isPlaying) {
+                              setState(() {
+                                _controller.pause();
+                              });
+                            } else {
+                              setState(() {
+                                _controller.play();
+                              });
+                            }
+                          },
+                          child: Container(
+                            height: 35,
+                            width: 35,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(65),
+                            ),
+                            child: _controller.value.isPlaying
+                                ? Icon(
+                                    Icons.pause,
+                                    color: AppColors.whitecolor,
+                                  )
+                                : Icon(
+                                    Icons.play_arrow,
+                                    color: AppColors.whitecolor,
+                                  ),
                           ),
-                          child: _controller.value.isPlaying
-                              ? Icon(
-                                  Icons.pause,
-                                  color: AppColors.whitecolor,
-                                )
-                              : Icon(
-                                  Icons.play_arrow,
-                                  color: AppColors.whitecolor,
-                                ),
-                        ),
-                      )))
-                    ],
-                  )),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 12),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    widget.workoutModel.workoutTitle.toString(),
-                    style: fontW5S12(context)!.copyWith(
-                        fontSize: 20,
-                        color: AppColors.blackcolor,
-                        fontWeight: FontWeight.w700),
-                  ),
-                  Text(
-                    widget.workoutModel.dateCreated!.toDate().format("d-M-Y"),
-                    style: fontW5S12(context)!.copyWith(
-                        fontSize: 12,
-                        color: AppColors.darkAppColor,
-                        fontWeight: FontWeight.w700),
-                  ),
-                ],
+                        )))
+                      ],
+                    )),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 12),
-              child: Text(
-                widget.workoutModel.workoutDuration.toString(),
-                style: fontW5S12(context)!.copyWith(
-                    fontSize: 15,
-                    color: AppColors.appcolor,
-                    fontWeight: FontWeight.w700),
+              SizedBox(
+                height: 10,
               ),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      widget.workoutModel.workoutTitle.toString(),
+                      style: fontW5S12(context)!.copyWith(
+                          fontSize: 20,
+                          color: AppColors.blackcolor,
+                          fontWeight: FontWeight.w700),
+                    ),
+                    Text(
+                      widget.workoutModel.dateCreated!.toDate().format("d-M-Y"),
+                      style: fontW5S12(context)!.copyWith(
+                          fontSize: 12,
+                          color: AppColors.darkAppColor,
+                          fontWeight: FontWeight.w700),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12),
+                child: Text(
+                  widget.workoutModel.workoutDuration.toString(),
+                  style: fontW5S12(context)!.copyWith(
+                      fontSize: 15,
+                      color: AppColors.appcolor,
+                      fontWeight: FontWeight.w700),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
